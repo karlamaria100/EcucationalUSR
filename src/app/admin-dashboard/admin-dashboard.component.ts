47c757f38cd151f5ab6ba47c857cae4e99d16a40
Rares Cristea <raresito@gmail.com>
2018-05-10 19:37:38 +0300

    Done Profile Page Added Admin Add Buttons Navigationa

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {SharedService} from "../shared.service";
+import {SharedService} from '../shared.service';
 
 @Component({
   selector: 'app-admin-deshboard',
@@ -10,20 +10,15 @@ export class AdminDeshboardComponent implements OnInit {
 
   studenti: any[] = [];
   asociatii: any[] = [];
-  constructor(private shared:SharedService) {
-    this.shared.getAllStudents().subscribe((value)=>{
+  constructor(private shared: SharedService) {
+    this.shared.getAllStudents().subscribe((value) => {
       console.log(value);
       this.studenti = value;
     });
-    // this.studenti.push({id: 1, email: 'karla', nume: 'Pantelimon', prenume: 'Karla', asociatie: 'ASMI', federatie: 'USR', id_facultate: '1407', consiliu: '0', senat: '0', consiliu_administratie: '0' });
-    // this.studenti.push({id: 2, email: 'rares', nume: 'Cristea', prenume: 'Rares', asociatie: 'ASMI', federatie: 'USR', id_facultate: '1407', consiliu: '0', senat: '1', consiliu_administratie: '0' });
-    this.shared.getAllAsociations().subscribe((value)=>{
+    this.shared.getAllAsociations().subscribe((value) => {
       console.log(value);
       this.asociatii = value;
     });
-    // this.asociatii.push({id: 25460950, denumire: 'Asociația Studenților la Matematică și Informatică', oras: 'București', jurisdictie: 'Facultatea de Matematică și Informatică'});
-    // this.asociatii.push({id: 1, denumire: 'Asociația Studenților în Administrație și Afaceri', oras: 'București', jurisdictie: 'Facultatea de Administrație și Afaceri'});
-
   }
 
   ngOnInit() {
@@ -31,9 +26,9 @@ export class AdminDeshboardComponent implements OnInit {
   }
 
 
-  openModal(s, action){
+  openModal(s, action) {
     console.log('Merge');
-    if(action == 1) {
+    if (action == 1) {
       const myModal = document.getElementById('editModal');
 
       (<HTMLInputElement>myModal.getElementsByClassName('nume').item(0)).value = s.nume;
@@ -43,7 +38,7 @@ export class AdminDeshboardComponent implements OnInit {
       (<HTMLInputElement>myModal.getElementsByClassName('facultate').item(0)).value = s.id_facultate;
 
       myModal.style.visibility = 'true';
-    }else if(action == 2){
+    }else if (action == 2) {
       const myModal = document.getElementById('editModalAsociatii');
 
       (<HTMLInputElement>myModal.getElementsByClassName('denumire').item(0)).value = s.denumire;
@@ -53,5 +48,12 @@ export class AdminDeshboardComponent implements OnInit {
       myModal.style.visibility = 'true';
     }
     return false;
+  }
+
+  openAddModal() {
+    const myModal = document.getElementById('addModal');
+    myModal.style.visibility = 'true';
+    return false;
+
   }
 }
